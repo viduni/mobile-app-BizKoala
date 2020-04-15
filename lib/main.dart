884eecc6f9f88d159c2c66135c0d2ffd0f838249
Viduni Randima <viduni@thesanmark.com>
2020-04-15 10:04:40 +0530

    Create User model and NotificationText widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/pages/login.dart';
+import 'package:mobile_app/views/login.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/',
---
+++ lib/models/user.dart
@@ -0,0 +1,6 @@
+class User{
+  final String email;
+  final String token;
+
+  User({this.email,this.token});
+}
